cd8c98bba902b7077e1fd67f11a655afff73af20
jose8af <josedavid8af@gmail.com>
2024-08-23 08:58:11 -0500

    desk

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,10 +1,13 @@
-return { 
-        'catppuccin/nvim', 
-        name = 'catppuccin', 
-        priority = 1000, 
-        config = function()
-                vim.cmd.colorscheme 'catppuccin-mocha'
-        end
-
+return {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup({
+            transparent_background = true,
+        })
+        vim.cmd.colorscheme "catppuccin"
+    end
 }
-        
+
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,12 @@
 return {
-        "nvim-treesitter/nvim-treesitter", 
-        build = ":TSUpdate", 
-        config = function() 
-                local configs = require("nvim-treesitter.configs")
-
-                configs.setup({
-                  ensure_installed = { "c", "lua", "vim", "javascript" },
-                  sync_install = false,
-                  highlight = { enable = true },
-                  indent = { enable = true },  
-                })
-        end
+    "nvim-treesitter/nvim-treesitter", 
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+        configs.setup({
+            ensure_installed = {"lua", "c", "javascript"},
+            highlight = { enable = true },
+            indent = { enable = true }
+        })
+    end
 }
- 
